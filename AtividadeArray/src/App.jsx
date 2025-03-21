9959094c2854a8fc673a8406d8453244454bdc67
João Victor <joaovictorpinheiro2007@gmail.com>
2025-03-21 20:08:03 -0300

    Add work of today

--- AtividadeArray/src/App.jsx
+++ AtividadeArray/src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
+  // Parte 1: arrays.js
+  console.log('Parte 1: arrays.js')
+  let frutas = ['laranja', 'banana', 'maçã', 'uva', 'abacaxi'];
+  console.log(frutas.length);
+  const terceiro_item = () => {
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {frutas[2]}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    );
+  }
+
+  // Parte 2: arrays.js
+  console.log('Parte 2: arrays.js');
+  let n = [1, 2, 3, 4, 5];
+  console.log(n);
+  n.pop();
+  console.log(n);
+  n.shift();
+  console.log(n);
+
+  // Parte 3: arrays.js
+  console.log('Parte 3: arrays.js');
+  let jogadores = [{ nome: "Pele", pontos: 10 }, { nome: "Maradona", pontos: 7 }, { nome: "Neymar", pontos: 8 }];
+  console.log(jogadores);
+  jogadores[0].pontos = 100;
+  console.log(jogadores);
+
+  // Parte 4: arrays.js
+  console.log('Parte 4: arrays.js');
+
+  let p = ['palavra', 'verbo', 'linguagem',];
+  let index = p.indexOf('linguagem');
+  if (index >= 0) {
+    console.log(index)
+  } else {
+    console.log('Palavra não encontrada')
+  }
+
+  // Parte 5: arrays.js
+  console.log('Parte 5: arrays.js');
+
+  let dados = [['João', 17, 'Goiânia'], ['Maria', 18, 'Goiânia'], ['Augusto', 17, 'Goiânia']];
+
+  console.log(dados[1][0] + ' da cidade de ' + dados[1][2]);
+
+  return (
+    <div className='container'>
+      <div className='body'>
+        <h1>Parte 1: arrays.js</h1>
+        {terceiro_item()}
+
+      </div>
+    </div>
   )
 }
 
